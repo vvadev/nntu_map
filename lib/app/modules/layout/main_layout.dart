b084c2c3a417a2596ff7afd81c8227aa5542b984
vvadev <vershinir@mail.ru>
2024-07-05 19:37:57 +0300

    added theme contoller and update API to 34

--- lib/app/modules/layout/main_layout.dart
+++ lib/app/modules/layout/main_layout.dart
@@ -1,6 +1,7 @@
 import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nntu_map/app/core/controllers/theme_controller.dart';
 
 import 'nav_controller.dart';
 
@@ -18,33 +19,39 @@ class MainLayout extends StatelessWidget {
     return GetBuilder<NavController>(
         init: NavController(),
         builder: (controller) {
-          return Scaffold(
-            appBar: appBar,
-            resizeToAvoidBottomInset: false,
-            extendBody: true,
-            body: enableSafeArea! ? SafeArea(child: child) : child,
-            bottomNavigationBar: CrystalNavigationBar(
-              currentIndex: navController.getCurrentIndex(),
-              onTap: (index) {
-                navController.changePage(index);
-              },
-              height: 10,
-              unselectedItemColor: Colors.white70,
-              backgroundColor: Colors.black.withOpacity(0.1),
-              items: [
-                CrystalNavigationBarItem(
-                  icon: Icons.list,
-                ),
-                CrystalNavigationBarItem(
-                  icon: Icons.map,
-                ),
-                CrystalNavigationBarItem(
-                  icon: Icons.apps,
-                ),
-                // Добавьте дополнительные элементы, если необходимо
-              ],
-            ),
-          );
+          return GetBuilder<ThemeController>(
+              init: ThemeController(),
+              builder: (themeController) {
+                return Scaffold(
+                  appBar: appBar,
+                  resizeToAvoidBottomInset: false,
+                  extendBody: true,
+                  body: enableSafeArea! ? SafeArea(child: child) : child,
+                  bottomNavigationBar: CrystalNavigationBar(
+                    currentIndex: navController.getCurrentIndex(),
+                    onTap: (index) {
+                      navController.changePage(index);
+                    },
+                    height: 10,
+                    unselectedItemColor: themeController.currentTheme == 'dark'
+                        ? Colors.white70
+                        : Colors.black54,
+                    backgroundColor: Colors.black.withOpacity(0.1),
+                    items: [
+                      CrystalNavigationBarItem(
+                        icon: Icons.list,
+                      ),
+                      CrystalNavigationBarItem(
+                        icon: Icons.map,
+                      ),
+                      CrystalNavigationBarItem(
+                        icon: Icons.apps,
+                      ),
+                      // Добавьте дополнительные элементы, если необходимо
+                    ],
+                  ),
+                );
+              });
         });
   }
 }
